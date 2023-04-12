f25031e81a1820ffe1f0b62b81761a539c08b273
bersyatina <aw_apanasenko@mail.ru>
2023-04-12 15:46:31 +0000

    redeploy railway

--- config/database.php
+++ config/database.php
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => 'postgresql://postgres:pXVkP69mZQuLWzGxp2BZ@containers-us-west-32.railway.app:7966/railway',
-            'host' => 'containers-us-west-32.railway.app',
-            'port' => '7966',
-            'database' => 'railway',
-            'username' => 'postgres',
-            'password' => 'pXVkP69mZQuLWzGxp2BZ',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
