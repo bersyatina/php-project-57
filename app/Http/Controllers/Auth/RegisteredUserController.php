114608acafc14f56a7afe1266bdfff43b04a7ca5
bersyatina <aw_apanasenko@mail.ru>
2023-04-14 04:27:42 +0000

    testing register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,13 +30,12 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        dd($request);
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
-
+        dd($request);
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
