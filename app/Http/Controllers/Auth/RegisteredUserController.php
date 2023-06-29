d92665670ad60a14a9ee68f5195436ea7b3bd9ab
bersyatina <aw_apanasenko@mail.ru>
2023-06-29 17:54:18 +0000

    fixed password validate bug

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,9 +32,9 @@ class RegisteredUserController extends Controller
     {
         $request->validate(
             [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
-            'password' => 'required|confirmed|string|min:8',
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
+                'password' => ['required', 'confirmed', 'string', 'min:8']
             ],
             $messages = [
                 'password.min' => 'Пароль должен иметь длину не менее 8 символов',
